a82808d4119d8661963008e73e6af0fcb3d1ad60
dusa2900 <goutham2118185@gmail.com>
2020-09-05 15:06:58 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { Routes,RouterModule} from '@angular/router';
+
+const approutes: Routes=[];
+
+@NgModule({
+imports: [RouterModule.forRoot(approutes)],
+exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
